958e63a9f48628ea714a15150305b5dd813d9281
Cynthia Raphaella <cynthia.raphaella@gmail.com>
2018-10-19 14:48:41 -0300

    Add a category dropdown

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_hours/data/hour.dart';
 import 'package:my_hours/pages/create_hour_page.dart';
+import 'package:my_hours/pages/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,54 +8,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Minhas horas',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       routes: <String, WidgetBuilder>{
         CreateHourPage.route: (BuildContext context) => CreateHourPage(),
       },
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Minhas horas'),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<Hour> _hours = List<Hour>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: _hours.map((h) => Text(h.getHourFormated(context))).toList()
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){_performAddNewHour(context);},
-        tooltip: 'Add hour',
-        child: Icon(Icons.add),
-      ), 
-    );
-  }
-
-  _performAddNewHour(context) {
-    Navigator.pushNamed(context, CreateHourPage.route);
-  }
-
 }
 
 
--- lib/pages/create_hour_page.dart
+++ lib/pages/create_hour_page.dart
@@ -9,40 +9,81 @@ class CreateHourPage extends StatefulWidget {
 
 class CreateHourPageState extends State<CreateHourPage> {
   Hour _hour = Hour.createByHour(TimeOfDay.now());
+  List<String> itens = ['primeira', 'segunda', 'terceira'];
+  String _selectedItem = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedItem = itens.elementAt(0);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: InkWell(
-          child: InputDecorator(
-            decoration: InputDecoration(
-              labelText: 'Select a time',
-              contentPadding: EdgeInsets.all(10.0),
-            ),
-            child: Row(
+        appBar: AppBar(
+          title: Text('Adicionar hora'),
+        ),
+        body: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
               children: <Widget>[
-                Text(_hour.getHourFormated(context)),
-                Icon(Icons.arrow_drop_down)
+                _buildTimePicker(),
+                _buildCategories()
               ],
-            ),
-          ),
-          onTap: () {_selectTime(context);},
-        )
-      )
+            )));
+  }
+
+  Widget _buildCategories() {
+    return InputDecorator(
+      decoration: InputDecoration(
+        labelText: 'Escolha uma categoria',
+        contentPadding: EdgeInsets.all(10.0),
+      ),
+      child: DropdownButton(
+        onChanged: (option) {
+          _selectCategory(option);
+        },
+        value: _selectedItem,
+        items: itens
+            .map((item) => DropdownMenuItem(child: Text(item), value: item))
+            .toList()));
+  }
+
+  void _selectCategory(option) {
+    setState(() {
+      _selectedItem = option;
+    });
+  }
+
+  Widget _buildTimePicker() {
+    return InkWell(
+      child: InputDecorator(
+        decoration: InputDecoration(
+          labelText: 'Escolha uma hora',
+          contentPadding: EdgeInsets.all(10.0),
+        ),
+        child: Row(
+          children: <Widget>[
+            Text(_hour.getHourFormated(context)),
+            Icon(Icons.arrow_drop_down)
+          ],
+        ),
+      ),
+      onTap: () {
+        _selectTime(context);
+      },
     );
   }
 
   Future<Null> _selectTime(BuildContext context) async {
-    final TimeOfDay picked = await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay.now()
-    );
+    final TimeOfDay picked =
+        await showTimePicker(context: context, initialTime: TimeOfDay.now());
 
     setState(() {
-       _hour.setHour(picked);
+      if(picked != null) {
+        _hour.setHour(picked);
+      }
     });
   }
 }
