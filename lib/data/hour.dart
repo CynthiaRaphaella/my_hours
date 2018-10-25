bc35221e2920d28cf78c6db20aa28fe346cc777e
Cynthia Raphaella <cynthia.raphaella@gmail.com>
2018-10-25 17:08:05 -0300

    Display hours on initial page based on firebase

--- lib/data/hour.dart
+++ lib/data/hour.dart
@@ -2,26 +2,16 @@ import 'package:flutter/material.dart';
 
 class Hour {
   DateTime _day;
-  TimeOfDay _hour;
 
   DateTime get day => _day;
-  TimeOfDay get hour => _hour;
 
-  void setDay(DateTime day) {
+  Hour(DateTime day) {
     _day = day;
   }
 
-  void setHour(TimeOfDay hour) {
-    _hour = hour;
-  }
-
   String getHourFormated(context) {
-    return _hour.format(context);
+    TimeOfDay time = TimeOfDay.fromDateTime(_day);
+    return time.format(context);
   }
 
-  Hour(this._day) {
-    _hour = TimeOfDay.fromDateTime(_day);
-  }
-
-  Hour.createByHour(this._hour);
 }
